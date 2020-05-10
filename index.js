24ff6b67d85859ccd9eecb4584a6bda5ce49cf66
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-10 10:23:15 -0700

    Update to return version names only

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 var semver = require('semver');
-var findLast = require('lodash.findlast');
-var filter = require('lodash.filter');
+var find = require('lodash.find');
 
 var parseExpression = require('./lib/parseExpression');
 var fetchHTTP = require('./lib/fetchHTTP');
 var fetchCache = require('./lib/fetchCache');
+var keyFunctions = require('./lib/keyFunctions');
 var normalizeSchedule = require('./lib/normalizeSchedule');
 var normalizeVersion = require('./lib/normalizeVersion');
-
-function minorKey(version) {
-  return version.major + '.' + version.minor;
-}
-function majorKey(version) {
-  return version.major;
-}
 
 function NodeVersions(versions, schedule) {
   if (!versions) throw new Error('Missing option: versions');
@@ -28,7 +21,7 @@ function NodeVersions(versions, schedule) {
   this.versions = [];
   for (var index = 0; index < versions.length; index++) this.versions.push(normalizeVersion(versions[index], this.schedules));
   this.versions = this.versions.sort(function (a, b) {
-    return semver.gt(a.version, b.version) ? 1 : -1;
+    return semver.gt(a.version, b.version) ? -1 : 1;
   });
 }
 
@@ -67,33 +60,32 @@ NodeVersions.prototype.resolve = function resolve(expression, options) {
   // single result, try a match
   var parsed = parseExpression.call(this, expression, options.now || new Date());
   if (parsed) {
-    var version = findLast(this.versions, function (x) {
+    var version = find(this.versions, function (x) {
       for (var key in parsed) {
         if (x[key] !== parsed[key]) return false;
       }
       return true;
     });
-    if (version) return version;
-  }
-
-  // unfiltered expression
-  if (typeof options.range === 'undefined' || options.range === 'patch') {
-    return filter(this.versions, function (version) {
-      return semver.satisfies(version.version, expression);
-    });
+    if (version) return version.name;
   }
 
   // filtered expression
+  var range = options.range || '';
+  var filters = { lts: !!~range.indexOf('lts') };
+  filters.key = ~range.indexOf('major') ? keyFunctions.major : ~range.indexOf('minor') ? keyFunctions.minor : undefined;
+
   var results = [];
   var founds = {};
-  var key = options.range === 'minor' ? minorKey : majorKey;
-  for (var index = this.versions.length - 1; index >= 0; index--) {
+  for (var index = 0; index < this.versions.length; index++) {
     // eslint-disable-next-line no-redeclare
     var version = this.versions[index];
+    if (filters.lts && !version.lts) continue;
     if (!semver.satisfies(version.version, expression)) continue;
-    if (founds[key(version)]) continue;
-    founds[key(version)] = true;
-    results.unshift(version);
+    if (filters.key) {
+      if (founds[filters.key(version)]) continue;
+      founds[filters.key(version)] = true;
+    }
+    results.unshift(version.name);
   }
   return results;
 };
