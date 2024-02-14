8a3246ba630065c7914808c1580c15a17b821aab
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-14 12:24:34 -0800

    Update to ts-dev-stack

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  CACHE_DIRECTORY: path.resolve(path.join(__dirname, '..', '.cache')),
+  CACHE_DIRECTORY: path.resolve(path.join(__dirname, '..', '..', '.cache')),
   DISTS_URL: 'https://nodejs.org/dist/index.json',
   SCHEDULES_URL: 'https://raw.githubusercontent.com/nodejs/Release/master/schedule.json',
 };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-var semver = require('semver');
-var Cache = require('fetch-json-cache');
+const semver = require('semver');
+const Cache = require('fetch-json-cache');
 
-var constants = require('./constants');
-var keyFunctions = require('./keyFunctions');
-var lineFunctions = require('./lineFunctions');
-var normalizeSchedule = require('./normalizeSchedule');
-var normalizeVersion = require('./normalizeVersion');
-var match = require('./match');
-var parseExpression = require('./parseExpression');
+const constants = require('./constants');
+const keyFunctions = require('./keyFunctions');
+const lineFunctions = require('./lineFunctions');
+const normalizeSchedule = require('./normalizeSchedule');
+const normalizeVersion = require('./normalizeVersion');
+const match = require('./match');
+const parseExpression = require('./parseExpression');
 
 function NodeVersions(versions, schedule) {
   if (!versions) throw new Error('Missing option: versions');
   if (!schedule) throw new Error('Missing option: schedule');
 
   this.schedules = [];
-  for (var name in schedule) this.schedules.push(normalizeSchedule(name, schedule[name]));
-  this.schedules = this.schedules.sort(function (a, b) {
-    return semver.gt(semver.coerce(a.semver), semver.coerce(b.semver)) ? 1 : -1;
-  });
+  for (const name in schedule) this.schedules.push(normalizeSchedule(name, schedule[name]));
+  this.schedules = this.schedules.sort((a, b) => (semver.gt(semver.coerce(a.semver), semver.coerce(b.semver)) ? 1 : -1));
 
   this.versions = [];
-  for (var index = 0; index < versions.length; index++) this.versions.push(normalizeVersion(versions[index], this.schedules));
-  this.versions = this.versions.sort(function (a, b) {
-    return semver.gt(a.semver, b.semver) ? -1 : 1;
-  });
+  for (let index = 0; index < versions.length; index++) this.versions.push(normalizeVersion(versions[index], this.schedules));
+  this.versions = this.versions.sort((a, b) => (semver.gt(a.semver, b.semver) ? -1 : 1));
 }
 
 NodeVersions.load = function load(options, callback) {
@@ -35,16 +31,16 @@ NodeVersions.load = function load(options, callback) {
   if (typeof callback === 'function') {
     options = options || {};
 
-    var cache = new Cache(options.cacheDirectory || constants.CACHE_DIRECTORY);
-    cache.get(constants.DISTS_URL, function (err, versions) {
+    const cache = new Cache(options.cacheDirectory || constants.CACHE_DIRECTORY);
+    cache.get(constants.DISTS_URL, (err, versions) => {
       if (err) return callback(err);
 
-      cache.get(constants.SCHEDULES_URL, function (err, schedule) {
+      cache.get(constants.SCHEDULES_URL, (err, schedule) => {
         err ? callback(err) : callback(null, new NodeVersions(versions, schedule));
       });
     });
   } else {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
       load(options, function loadCallback(err, NodeVersions) {
         err ? reject(err) : resolve(NodeVersions);
       });
@@ -54,28 +50,28 @@ NodeVersions.load = function load(options, callback) {
 
 NodeVersions.loadSync = function loadSync(options) {
   options = options || {};
-  var cache = new Cache(options.cacheDirectory || constants.CACHE_DIRECTORY);
-  var versions = cache.getSync(constants.DISTS_URL);
-  var schedule = cache.getSync(constants.SCHEDULES_URL);
+  const cache = new Cache(options.cacheDirectory || constants.CACHE_DIRECTORY);
+  const versions = cache.getSync(constants.DISTS_URL);
+  const schedule = cache.getSync(constants.SCHEDULES_URL);
   if (!versions || !schedule) return null;
   return new NodeVersions(versions, schedule);
 };
 
 NodeVersions.prototype.resolve = function resolve(expression, options) {
   options = options || {};
-  var path = options.path || 'version';
+  const path = options.path || 'version';
 
   // normalize
-  if (typeof expression === 'number') expression = '' + expression;
+  if (typeof expression === 'number') expression = `${expression}`;
   if (typeof expression !== 'string') return null;
   expression = expression.trim();
 
   // single result, try a match
-  var query = parseExpression.call(this, expression, options.now || new Date());
+  const query = parseExpression.call(this, expression, options.now || new Date());
   if (query) {
-    var version = null;
-    for (var index = 0; index < this.versions.length; index++) {
-      var test = this.versions[index];
+    let version = null;
+    for (let index = 0; index < this.versions.length; index++) {
+      const test = this.versions[index];
       if (options.now && options.now < test.date) continue;
       if (!match(test, query)) continue;
       version = test;
@@ -85,17 +81,16 @@ NodeVersions.prototype.resolve = function resolve(expression, options) {
   }
 
   // filtered expression
-  var range = options.range || '';
-  var filters = { lts: !!~range.indexOf('lts') };
+  const range = options.range || '';
+  const filters = { lts: !!~range.indexOf('lts') };
   filters.key = ~range.indexOf('major') ? keyFunctions.major : ~range.indexOf('minor') ? keyFunctions.minor : undefined;
   filters.line = ~range.indexOf('even') ? lineFunctions.even : ~range.indexOf('odd') ? lineFunctions.odd : undefined;
 
-  var results = [];
-  var founds = {};
-  // eslint-disable-next-line no-redeclare
-  for (var index = 0; index < this.versions.length; index++) {
-    // eslint-disable-next-line no-redeclare
-    var test = this.versions[index];
+  const results = [];
+  const founds = {};
+
+  for (let index = 0; index < this.versions.length; index++) {
+    const test = this.versions[index];
     if (options.now && options.now < test.date) continue;
     if (filters.lts && !test.lts) continue;
     if (filters.line && !filters.line(test)) continue;
