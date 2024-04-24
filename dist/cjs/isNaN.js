c92cd432b2768497b0fb4c783abc5442507c051c
Kevin Malakoff <kmalakoff@gmail.com>
2024-04-24 12:18:21 -0700

    Upgrade modules

--- dist/cjs/isNaN.js
+++ dist/cjs/isNaN.js
@@ -4,9 +4,4 @@ module.exports = function isNaN(value) {
     // biome-ignore lint/suspicious/noSelfCompare: <explanation>
     return value !== value;
 };
-
-if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
-  Object.defineProperty(exports.default, '__esModule', { value: true });
-  for (var key in exports) exports.default[key] = exports[key];
-  module.exports = exports.default;
-}
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
