cdad3fd4c4bd60473588c65b349d0e5b3c67e271
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-03 07:57:06 -0800

    Upgrade modules

--- dist/cjs/keyFunctions.js
+++ dist/cjs/keyFunctions.js
@@ -5,4 +5,4 @@ module.exports.major = function major(version) {
 module.exports.minor = function minor(version) {
     return version.major === 0 ? "".concat(version.major, ".").concat(version.minor, ".").concat(version.patch) : "".concat(version.major, ".").concat(version.minor);
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
--- dist/cjs/lineFunctions.js
+++ dist/cjs/lineFunctions.js
@@ -7,4 +7,4 @@ module.exports.odd = function odd(version) {
     var major = version.major === 0 ? version.minor : version.major;
     return major % 2 === 1;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
