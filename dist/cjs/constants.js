9ea65cde507a12a232010113ad41e933312a489e
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 19:01:23 -0800

    Upgrade modules

--- dist/cjs/constants.js
+++ dist/cjs/constants.js
@@ -5,4 +5,4 @@ module.exports = {
     DISTS_URL: 'https://nodejs.org/dist/index.json',
     SCHEDULES_URL: 'https://raw.githubusercontent.com/nodejs/Release/master/schedule.json'
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
--- dist/cjs/normalizeSchedule.js
+++ dist/cjs/normalizeSchedule.js
@@ -12,4 +12,4 @@ module.exports = function normalizeSchedule(name, raw) {
     if (raw.maintenance) schedule.maintenance = new Date(raw.maintenance);
     return schedule;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
