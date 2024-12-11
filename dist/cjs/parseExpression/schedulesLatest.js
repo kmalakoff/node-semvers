c525e205f52f12fb5e5280f1a9553ce23254751c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:30 -0800

    Upgrade modules

--- dist/cjs/parseExpression/schedulesLatest.js
+++ dist/cjs/parseExpression/schedulesLatest.js
@@ -7,4 +7,4 @@ module.exports = function schedulesLatest(schedules, filter) {
     });
     return latest;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
