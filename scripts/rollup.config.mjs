07fc0c7be3c63364ace4cb58bf899483ba2471c5
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-25 02:41:28 -0800

    Upgrade modules

--- scripts/rollup.config.mjs
+++ scripts/rollup.config.mjs
@@ -6,7 +6,7 @@ import path from 'path';
 import url from 'url';
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
-import getTS from 'get-tsconfig-compat';
+import * as getTS from 'get-tsconfig-compat';
 const tsconfig = getTS.getTsconfig(path.resolve(__dirname, '..', 'tsconfig.json'));
 tsconfig.config.compilerOptions.target = 'ES4';
 
