859b49999a7a40d9d14708eed82f6af12c87b8ff
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 15:23:54 -0800

    Upgrade modules

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,3 +1,20 @@
-#!/usr/bin/env node
+var Cache = require('fetch-json-cache');
+var constants = require('../assets/constants.cjs');
 
-require('../assets/postinstall.cjs')();
+function cache(callback) {
+  var cache = new Cache(constants.CACHE_PATH);
+  cache.get(constants.DISTS_URL, { force: true }, function (err) {
+    err ? callback(err) : cache.get(constants.SCHEDULES_URL, { force: true }, callback);
+  });
+}
+
+// run patch
+cache(function (err) {
+  if (err) {
+    console.log('postinstall failed. Error: ' + err.message);
+    process.exit(-1);
+  } else {
+    console.log('postinstall succeeded');
+    process.exit(0);
+  }
+});
