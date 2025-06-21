9dfd7dc91cf161576099a8dcb12327eedde337a9
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 00:16:20 -0700

    Upgrade modules

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
-// @ts-ignore
-export { default } from '../../assets/constants.cjs';
+import Module from 'module';
+
+const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
+const path = '../../assets/constants.cjs';
+export default _require(path);
