8a1a7fb3746410bd9d49fab1fbfe3b0194c08398
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 21:23:00 -0800

    Upgrade modules

---
+++ src/scripts/postinstall.ts
@@ -0,0 +1,37 @@
+import exit from 'exit-compat';
+import Cache from 'fetch-json-cache';
+import { CACHE_PATH, DISTS_URL, SCHEDULES_URL } from '../constants.ts';
+
+type Callback = (err: Error | null) => void;
+
+function fetchWithRetry(cache: Cache, url: string, retries: number, callback: Callback): void {
+  cache.get(url, { force: true }, (err: Error | null) => {
+    if (err && retries > 0) {
+      setTimeout(() => {
+        fetchWithRetry(cache, url, retries - 1, callback);
+      }, 1000);
+    } else {
+      callback(err);
+    }
+  });
+}
+
+function cacheWithRetry(callback: Callback): void {
+  const cache = new Cache(CACHE_PATH);
+  fetchWithRetry(cache, DISTS_URL, 3, (err) => {
+    if (err) return callback(err);
+    fetchWithRetry(cache, SCHEDULES_URL, 3, callback);
+  });
+}
+
+// run patch
+cacheWithRetry((err) => {
+  if (err) {
+    console.log(`postinstall warning: ${err.message}`);
+    console.log('Cache not updated after retries.');
+    exit(0);
+  } else {
+    console.log('postinstall succeeded');
+    exit(0);
+  }
+});
