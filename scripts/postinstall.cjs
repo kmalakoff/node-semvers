756a40bcbd1f96cedabf2864be6147b140148089
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 08:57:16 -0700

    Refactor postinstall script

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,34 +1,9 @@
-var Cache = require('fetch-json-cache');
-var constants = require('../assets/constants.cjs');
-
-function fetchWithRetry(cache, url, retries, callback) {
-  cache.get(url, { force: true }, function (err) {
-    if (err && retries > 0) {
-      setTimeout(function () {
-        fetchWithRetry(cache, url, retries - 1, callback);
-      }, 1000);
-    } else {
-      callback(err);
-    }
-  });
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var compiled = path.join(__dirname, '..', 'dist', 'cjs', 'scripts', 'postinstall.js');
+if (fs.existsSync(compiled)) {
+  require(compiled);
+} else {
+  console.log('postinstall: Skipping (dist/ not built yet - run npm run build)');
 }
-
-function cacheWithRetry(callback) {
-  var cache = new Cache(constants.CACHE_PATH);
-  fetchWithRetry(cache, constants.DISTS_URL, 3, function (err) {
-    if (err) return callback(err);
-    fetchWithRetry(cache, constants.SCHEDULES_URL, 3, callback);
-  });
-}
-
-// run patch
-cacheWithRetry(function (err) {
-  if (err) {
-    console.log('postinstall warning: ' + err.message);
-    console.log('Cache not updated after retries.');
-    process.exit(0);
-  } else {
-    console.log('postinstall succeeded');
-    process.exit(0);
-  }
-});
